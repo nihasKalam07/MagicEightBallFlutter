aec1e9926f2899c5d38a6b9b1e2ed5bc579b71ff
Nihas Kalam <nihasna@gmail.com>
2022-12-17 20:48:15 +0000

    initial commit

---
+++ android/app/src/main/kotlin/com/nihaskalam/magic_8_ball_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nihaskalam.magic_8_ball_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.lightBlue,
+          appBar: AppBar(
+            backgroundColor: Colors.blue.shade900,
+            title: const Text("Ask me anything"),
+            centerTitle: true,
+          ),
+          body: const MagicBallPage(),
+        ),
+      ),
+    );
+
+class MagicBallPage extends StatefulWidget {
+  const MagicBallPage({Key? key}) : super(key: key);
+
+  @override
+  State<MagicBallPage> createState() => _MagicBallPageState();
+}
+
+class _MagicBallPageState extends State<MagicBallPage> {
+  int ballNumber = 1;
+
+  void changeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      if (kDebugMode) {
+        print(ballNumber);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextButton(
+          onPressed: () {
+            changeBallNumber();
+          },
+          child: Image.asset('images/ball$ballNumber.png'),
+        ),
+      ),
+    );
+  }
+}
